1f946ea0746fb0cfe392c34c37087b56973488b9
Yonatan Schrift <yonatan.schrift@gmail.com>
2025-09-26 00:11:59 +0300

    Edited install.bat to check for python, and echo information to the user.

--- install.bat
+++ install.bat
@@ -1,4 +1,24 @@
+@echo off
+REM --- Check for Python ---
+python --version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Python isn't installed, please install Python 3.11 or later.
+    goto :end
+)
+
+REM --- Create virtual environment ---
+echo Creating a virtual Python environment for the project...
 python -m venv venv
+
+REM --- Activate venv ---
 call venv\Scripts\activate
-pip install -r requirements.txt
-playwright install
+
+REM --- Install requirements ---
+echo Installing project requirements...
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m playwright install
+
+echo Successfully installed the project requirements!
+:end
+pause
