e738113fa99984025e78a46218ea16ecf7891ad4
Yonatan Schrift <yonatan.schrift@gmail.com>
2025-09-26 00:18:00 +0300

    I upload files wayyy too fast, updated install.bat again to be nicer.

--- install.bat
+++ install.bat
@@ -2,23 +2,30 @@
 REM --- Check for Python ---
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo Python isn't installed, please install Python 3.11 or later.
+    echo -!- Python isn't installed, please install Python 3.11 or later.
     goto :end
 )
 
 REM --- Create virtual environment ---
-echo Creating a virtual Python environment for the project...
+echo --- Creating a virtual Python environment for the project...
 python -m venv venv
 
 REM --- Activate venv ---
 call venv\Scripts\activate
 
 REM --- Install requirements ---
-echo Installing project requirements...
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m playwright install
+echo --- Installing project requirements...
+python -m pip install --upgrade pip >nul
+python -m pip install -r requirements.txt >nul
+python -m playwright install >nul
 
-echo Successfully installed the project requirements!
+echo.
+echo ✅ Successfully installed the project requirements!
+echo.
+echo Run the script with:   python main.py
+echo.
+echo ⚠ Don't forget to update the environment file with your credentials!
+echo.
+
 :end
 pause
